3c9e73338b7895b90ad5132b24fff927ed4e3777
SultanovRustam <rust17@yandex.ru>
2022-12-17 07:06:34 +0300

    fix bag

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -12,12 +12,9 @@ router.register('tags', TagViewSet)
 router.register('ingredients', IngredientViewSet)
 router.register('recipes', RecipeViewSet, basename='recipe')
 
+
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
     path('recipes/<int:favorite_id>/favorite/', FavoriteView.as_view()),
-    path('users/subscriptions/', SubscriptionViewSet.as_view()),
-    path('users/<int:pk>/subscribe/', SubscribeView.as_view()),
 
 ]
